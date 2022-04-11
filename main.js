424bbe66e30e9f5009961c4e3454a12b12feec3a
hasita-art <73806182+hasita-art@users.noreply.github.com>
2022-04-10 22:32:39 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 song1 = "";
 song2 = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
 
 function preload(){
     song1 = loadSound("gogo.mp3");
@@ -12,6 +16,27 @@ function setup(){
 
     video.createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose", gotPoses);
+}
+
+function modelLoaded() {
+    console.log("PoseNet is initialized");
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX + "leftWristY = " + leftwristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log(
+        "rightWristX = " + rightWristX + "rightWristY = " + rightWristY);
+    }
 }
 
 function draw(){
